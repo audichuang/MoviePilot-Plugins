a8074c97f37c493fdfc4260b55273af2957c893a
audichuang <audichuang@icloud.com>
2024-05-08 13:54:51 +0800

    fix

--- plugins/libraryscraper_tw/__init__.py
+++ plugins/libraryscraper_tw/__init__.py
@@ -28,7 +28,7 @@ class LibraryScraper_Tw(_PluginBase):
     # 插件图标
     plugin_icon = "scraper.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "audichuang"
     # 作者主页
