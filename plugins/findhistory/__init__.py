56d8e5ba6fc341a84f0f9a541012ac1a6e0b8564
audichuang <audichuang@icloud.com>
2024-05-09 10:57:46 +0800

    fix

--- plugins/findhistory/__init__.py
+++ plugins/findhistory/__init__.py
@@ -15,7 +15,7 @@ class FindHistory(_PluginBase):
     # 插件图标
     plugin_icon = "Bookstack_A.png"
     # 插件版本
-    plugin_version = "0.1"
+    plugin_version = "0.2"
     # 插件作者
     plugin_author = "audichuang"
     # 作者主页
