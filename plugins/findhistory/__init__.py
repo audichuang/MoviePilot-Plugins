ae0ff9c82cfff22c9bd29154c84c7d29cfd77df4
audichuang <audichuang@icloud.com>
2024-05-09 12:26:04 +0800

    init

--- plugins/findhistory/__init__.py
+++ plugins/findhistory/__init__.py
@@ -2,6 +2,7 @@ import sqlite3
 from pathlib import Path
 from typing import List, Tuple, Dict, Any
 
+import datetime
 from app.core.config import Settings
 from app.log import logger
 from app.plugins import _PluginBase
@@ -15,7 +16,7 @@ class FindHistory(_PluginBase):
     # 插件图标
     plugin_icon = "Bookstack_A.png"
     # 插件版本
-    plugin_version = "0.2"
+    plugin_version = "0.3"
     # 插件作者
     plugin_author = "audichuang"
     # 作者主页
@@ -42,7 +43,7 @@ class FindHistory(_PluginBase):
         self.__update_config()
 
     def _task(self):
-        db_path = Settings().CONFIG_PATH / 'user.db'
+        db_path = Settings().CONFIG_PATH / "user.db"
         logger.info(f"数据库文件路径：{db_path}")
         try:
             gradedb = sqlite3.connect(db_path)
@@ -54,25 +55,25 @@ class FindHistory(_PluginBase):
             # 创建游标cursor来执行executeＳＱＬ语句
             logger.info(f"連接到数据库 {db_path}")
             cursor = gradedb.cursor()
-            sql = '''
-                    SELECT
-                        src,
-                        dest,
-                        type,
-                        category,
-                        tmdbid,
-                        year,
-                        date
-                    FROM
-                        transferhistory  
-                    WHERE
-                        src IS NOT NULL
-                        AND dest IS NOT NULL
-                        AND date >= DATE_SUB(NOW(), INTERVAL 5 DAY)
-                        '''
+            # 获取当前日期
+            today = datetime.date.today()
+
+            # 计算5天前的日期
+            five_days_ago = today - datetime.timedelta(days=5)
+
+            # 将日期格式化为字符串
+            five_days_ago_str = five_days_ago.strftime("%Y-%m-%d")
+
+            sql = f"""
+            SELECT src, dest, type, category, tmdbid, year, date
+            FROM transferhistory
+            WHERE src IS NOT NULL
+            AND dest IS NOT NULL
+            AND date >= '{five_days_ago_str}'
+            """
             cursor.execute(sql)
             transfer_history += cursor.fetchall()
-            
+
             logger.info(f"查询到历史记录{len(transfer_history)}条")
             cursor.close()
 
@@ -82,13 +83,13 @@ class FindHistory(_PluginBase):
             transfer_history = []
             for row in cursor.fetchall():
                 transfer_dict = {
-                    'src': row[0],
-                    'dest': row[1],
-                    'type': row[2],
-                    'category': row[3],
-                    'tmdbid': row[4],
-                    'year': row[5],
-                    'date': row[6]
+                    "src": row[0],
+                    "dest": row[1],
+                    "type": row[2],
+                    "category": row[3],
+                    "tmdbid": row[4],
+                    "year": row[5],
+                    "date": row[6],
                 }
                 transfer_history.append(transfer_dict)
             # 使用logger.info逐行输出查询结果
@@ -118,10 +119,7 @@ class FindHistory(_PluginBase):
         logger.info("全部处理完成")
 
     def __update_config(self):
-        self.update_config({
-            "onlyonce": self._onlyonce,
-            "link_dirs": self._link_dirs
-        })
+        self.update_config({"onlyonce": self._onlyonce, "link_dirs": self._link_dirs})
 
     @staticmethod
     def get_command() -> List[Dict[str, Any]]:
@@ -133,79 +131,73 @@ class FindHistory(_PluginBase):
     def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
         return [
             {
-                'component': 'VForm',
-                'content': [
+                "component": "VForm",
+                "content": [
                     {
-                        'component': 'VRow',
-                        'content': [
+                        "component": "VRow",
+                        "content": [
                             {
-                                'component': 'VCol',
-                                'props': {
-                                    'cols': 12,
-                                    'md': 6
-                                },
-                                'content': [
+                                "component": "VCol",
+                                "props": {"cols": 12, "md": 6},
+                                "content": [
                                     {
-                                        'component': 'VSwitch',
-                                        'props': {
-                                            'model': 'onlyonce',
-                                            'label': '立即运行一次',
-                                        }
+                                        "component": "VSwitch",
+                                        "props": {
+                                            "model": "onlyonce",
+                                            "label": "立即运行一次",
+                                        },
                                     }
-                                ]
+                                ],
                             }
-                        ]
+                        ],
                     },
                     {
-                        'component': 'VRow',
-                        'content': [
+                        "component": "VRow",
+                        "content": [
                             {
-                                'component': 'VCol',
-                                'props': {
-                                    'cols': 12,
+                                "component": "VCol",
+                                "props": {
+                                    "cols": 12,
                                 },
-                                'content': [
+                                "content": [
                                     {
-                                        'component': 'VTextarea',
-                                        'props': {
-                                            'model': 'link_dirs',
-                                            'label': '需要恢复的硬链接目录',
-                                            'rows': 5,
-                                            'placeholder': '硬链接目录 （一行一个）'
-                                        }
+                                        "component": "VTextarea",
+                                        "props": {
+                                            "model": "link_dirs",
+                                            "label": "需要恢复的硬链接目录",
+                                            "rows": 5,
+                                            "placeholder": "硬链接目录 （一行一个）",
+                                        },
                                     }
-                                ]
+                                ],
                             }
-                        ]
+                        ],
                     },
                     {
-                        'component': 'VRow',
-                        'content': [
+                        "component": "VRow",
+                        "content": [
                             {
-                                'component': 'VCol',
-                                'props': {
-                                    'cols': 12,
+                                "component": "VCol",
+                                "props": {
+                                    "cols": 12,
                                 },
-                                'content': [
+                                "content": [
                                     {
-                                        'component': 'VAlert',
-                                        'props': {
-                                            'type': 'info',
-                                            'variant': 'tonal',
-                                            'text': '根据转移记录中的硬链接恢复源文件',
-                                            'style': 'white-space: pre-line;'
-                                        }
+                                        "component": "VAlert",
+                                        "props": {
+                                            "type": "info",
+                                            "variant": "tonal",
+                                            "text": "根据转移记录中的硬链接恢复源文件",
+                                            "style": "white-space: pre-line;",
+                                        },
                                     }
-                                ]
+                                ],
                             }
-                        ]
-                    }
-                ]
+                        ],
+                    },
+                ],
             }
-        ], {
-            "onlyonce": False,
-            "link_dirs": ""
-        }
+        ], {"onlyonce": False, "link_dirs": ""}
 
     def get_page(self) -> List[dict]:
         pass
