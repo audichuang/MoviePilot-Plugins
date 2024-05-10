144a10c88c292213d1b14c49c09fa75fe399eae0
audichuang <audichuang@icloud.com>
2024-05-10 15:20:52 +0800

    fix

--- plugins/tidy/__init__.py
+++ plugins/tidy/__init__.py
@@ -15,7 +15,7 @@ class Tidy(_PluginBase):
     # 插件图标
     plugin_icon = "Bookstack_A.png"
     # 插件版本
-    plugin_version = "1.5"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "audichuang"
     # 作者主页
@@ -230,7 +230,7 @@ class Tidy(_PluginBase):
                     },
                 ],
             }
-        ], {"onlyonce": False}
+        ], {"onlyonce": False, "notify": False}
 
     def get_page(self) -> List[dict]:
         pass
