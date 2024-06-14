fc3f3c61a3e9dbc3df635a525a2e91ec836783aa
audichuang <audichuang@icloud.com>
2024-06-14 12:41:02 +0800

    fix

--- plugins/refreshepisode/__init__.py
+++ plugins/refreshepisode/__init__.py
@@ -33,7 +33,7 @@ class RefreshEpisode(_PluginBase):
     # 插件图标
     plugin_icon = "Bookstack_A.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "audichuang"
     # 作者主页
