47313f2eb6172ef4e6b302982b98db93132c8fe6
audichuang <audichuang@icloud.com>
2024-06-17 14:37:45 +0800

    fix

--- plugins/mediascraperone/__init__.py
+++ plugins/mediascraperone/__init__.py
@@ -26,7 +26,7 @@ class MediaScraperOne(_PluginBase):
     # 插件图标
     plugin_icon = "scraper.png"
     # 插件版本
-    plugin_version = "0.1"
+    plugin_version = "0.3"
     # 插件作者
     plugin_author = "audichuang"
     # 作者主页
